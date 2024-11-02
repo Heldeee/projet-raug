00c9652f4d8769c0a2538e6c8ab03faa5ac6b617
Leo Devin <leo.devin@epita.fr>
2024-11-02 13:31:31 +0100

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/projet_raug/' : '/',
+  base: '/projet_raug/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
